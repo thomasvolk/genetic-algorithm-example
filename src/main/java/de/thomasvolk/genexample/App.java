0eb9edb1353d16b852e5cb23323bb4c47e65f8df
Thomas Volk (thomas-VB1) <thomas@my-volk.de>
2015-04-06 17:50:10 +0200

    init

---
+++ src/main/java/de/thomasvolk/genexample/App.java
@@ -0,0 +1,13 @@
+package de.thomasvolk.genexample;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
