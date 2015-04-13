3ec23331db4fd146dbf0099ae42dc1dc473cb3be
Thomas Volk (thomas-VB1) <thomas@my-volk.de>
2015-04-13 23:31:10 +0200

    messung zufreidenheit

--- src/test/java/de/thomasvolk/genexample/algorithm/FirstComeFirstServedTest.java
+++ src/test/java/de/thomasvolk/genexample/algorithm/FirstComeFirstServedTest.java
@@ -14,6 +14,7 @@ import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertEquals;
 
 public class FirstComeFirstServedTest extends AbstractWagonTest {
 
@@ -29,11 +30,14 @@ public class FirstComeFirstServedTest extends AbstractWagonTest {
 
     @Test
     public void einfacheWertungen() {
-        List<Passagier> passagiere = getPassagiere(0, ANZAHL_SITZPLAETZE, new Wertung(Wertung.EINFACHE_GEWICHTUNG,Wertung.EINFACHE_GEWICHTUNG,Wertung.EINFACHE_GEWICHTUNG));
+        List<Passagier> passagiere = getPassagiere(0, ANZAHL_SITZPLAETZE,
+                new Wertung(Wertung.EINFACHE_GEWICHTUNG,Wertung.EINFACHE_GEWICHTUNG,Wertung.EINFACHE_GEWICHTUNG));
         FirstComeFirstServed algorithmus = new FirstComeFirstServed(
                 getSitzPlaetze().toArray(new Sitzplatz[getSitzPlaetze().size()]),
                 passagiere.toArray(new Passagier[passagiere.size()]));
+        assertEquals(2500, new Wagon(getSitzPlaetze(), passagiere).getZufriedenheit(), 0);
         assertArrayEquals(new int[]{12, 14, 0, 2, 6, 8, 9, 11, 13, 1, 3, 5, 7, 10, 4}, algorithmus.getPassagierReihenfolge());
+        assertEquals(2500, new Wagon(getSitzPlaetze(), passagiere, algorithmus.getPassagierReihenfolge()).getZufriedenheit(), 0);
     }
 
 
@@ -44,7 +48,9 @@ public class FirstComeFirstServedTest extends AbstractWagonTest {
                 getSitzPlaetze().toArray(new Sitzplatz[getSitzPlaetze().size()]),
                 passagiere.toArray(new Passagier[passagiere.size()]));
         int[] passagierReihenfolge = algorithmus.getPassagierReihenfolge();
+        assertEquals(400, new Wagon(getSitzPlaetze(), passagiere).getZufriedenheit(), 0);
         assertArrayEquals(new int[]{12, 0, 2, 1, 3, 4, 5, 6, 7, 8, 9, 10, 11, 13, 14}, passagierReihenfolge);
+        assertEquals(600, new Wagon(getSitzPlaetze(), passagiere, algorithmus.getPassagierReihenfolge()).getZufriedenheit(), 0);
     }
 
 }
