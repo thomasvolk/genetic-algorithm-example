5aca054019be1d7d4e9c3d48a25c4260339ffc98
Thomas Volk (thomas-VB1) <thomas@my-volk.de>
2015-05-09 20:24:29 +0200

    add fat jar

--- src/main/java/de/thomasvolk/genexample/Main.java
+++ src/main/java/de/thomasvolk/genexample/Main.java
@@ -1,51 +1,8 @@
 package de.thomasvolk.genexample;
 
-import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
-import javafx.geometry.Insets;
-import javafx.geometry.Pos;
-import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.layout.GridPane;
-import javafx.scene.layout.StackPane;
-import javafx.stage.FileChooser;
-import javafx.stage.Stage;
-
-public class Main extends Application {
+public class Main {
     public static void main( String[] args ) {
-         launch(args);
+         System.out.println("Genetische Algorithmen");
     }
 
-    @Override
-    public void start(Stage primaryStage) {
-        primaryStage.setTitle("Hello World!");
-        Label label = new Label();
-        label.setText("hallo");
-        Button btn = new Button();
-        btn.setText("Say 'Hello World'");
-        btn.setOnAction(new EventHandler<ActionEvent>() {
-
-            @Override
-            public void handle(ActionEvent event) {
-                FileChooser fileChooser = new FileChooser();
-                fileChooser.setTitle("Open Resource File");
-                fileChooser.showOpenDialog(primaryStage);
-            }
-        });
-
-        GridPane grid = new GridPane();
-        grid.setAlignment(Pos.CENTER);
-        grid.setHgap(10);
-        grid.setVgap(10);
-        grid.setPadding(new Insets(25, 25, 25, 25));
-        grid.add(label, 1, 1);
-        grid.add(btn, 2, 1);
-
-        StackPane root = new StackPane();
-        root.getChildren().add(grid);
-        primaryStage.setScene(new Scene(root, 300, 250));
-        primaryStage.show();
-    }
 }
